f4556111b9b37f128d54b759740690ca5c98da1a
beijilang <35845122+beijilang@users.noreply.github.com>
2018-07-20 10:35:26 -0400

    Add files via upload

---
+++ src/com/mygdx/finalgame/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mygdx.finalgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.finalgame.BoxHead;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1000;
+		config.height = 1000;
+		new LwjglApplication(new BoxHead(), config);
+	}
+}
